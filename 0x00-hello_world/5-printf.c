2751fed254cb43ad25dd127638020baacdfd4126
ramii02 <louhibirami11@gmail.com>
2021-02-03 14:42:45 +0100

    cava

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * print msg with printf function
+ * Return: success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
