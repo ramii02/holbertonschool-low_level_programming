e9073f10e9a112185c200d9c5e22b9f502aacdad
ramii02 <louhibirami11@gmail.com>
2021-02-04 20:05:12 +0100

    hi

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main-entry point
+ * print alpha
+ * Returns: success
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
