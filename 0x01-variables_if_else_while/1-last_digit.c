18160f14215582b822eb720e725641e9d00f7058
ramii02 <louhibirami11@gmail.com>
2021-02-04 15:04:42 +0100

    fix

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/*
+* main - assigns a random number to int n everytime
+* it executes, and prints it
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n % 10 > 5)
+printf("and is greater than 5\n");
+else if (n % 10 == 0);
+printf("and is 0\n");
+else if (n % 10 < 6 && n % 10 != 0)
+printf("and is less than 6 and not 0\n");
+return (0);
+}
