9e9046be808acf4f4a7379bce21a1d5ccaf08c99
ramii02 <louhibirami11@gmail.com>
2021-02-09 21:22:09 +0100

    fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
-* * main -  prints Holberton, followed by a new line
-* * Return: Always 0 (Success)
+* main -  prints Holberton, followed by a new line
+* Return: Always 0 (Success)
 */
 int main(void)
 {
