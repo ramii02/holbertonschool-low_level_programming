9a30503cecf3e90d44729aefd7ecdc02e774167d
ramii02 <louhibirami11@gmail.com>
2021-02-09 11:17:14 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - prints Holberton, followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+
+{
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+return (0);
+}
