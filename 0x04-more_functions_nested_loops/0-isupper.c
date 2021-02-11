97d0324d1865dd55b36842b43601b66aad283575
ramii02 <louhibirami11@gmail.com>
2021-02-11 11:24:43 +0100

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - check the code for Holberton School students.
+* @c: checks for uppercase character
+* Return: Always 0.
+*/
+int _isupper(int c);
+
+{
+if ('% d, c > 65 && % d, c > 90')
+return (1)
+else
+return (0)
+}
