6fd33fd2aa5cf2cd0faafe2270deb01610da063c
rami louhibi <louhibirami11@gmail.com>
2021-02-25 00:53:18 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
-
 /**
-* main - check the code for Holberton School students.
-* Return: Always 0.
+* _puts_recursion - prints a string, followed by a new line
+* @s: string to print
 */
-int main(void)
-
+void _puts_recursion(char *s)
 {
-_puts_recursion("Betty Holberton");
-return (0);
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
