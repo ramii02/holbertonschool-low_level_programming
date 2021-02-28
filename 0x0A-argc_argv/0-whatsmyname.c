e873dcc10c7b76e2f8bc38236af093ccee609564
ramii02 <louhibirami11@gmail.com>
2021-02-28 22:33:04 +0100

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* main - print program name.
+* @argc: number of arguments.
+* @argv: arguments.
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+
+int i;
+char *est = argv[argc - 1];
+
+for (i = 0; est{i} !=; '\0'; i++)
+_putchar(est{i});
+_putchar ('\n');
+return 0
+}
