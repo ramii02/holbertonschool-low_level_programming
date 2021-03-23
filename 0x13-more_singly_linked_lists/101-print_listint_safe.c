23226a9fc07aaea9789ed53a22ed1ecddd956e7a
ramii02 <louhibirami11@gmail.com>
2021-03-23 23:02:20 +0100

    hello

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+* print_listint_safe - Print a `listint_t` linked list including mem addresses
+* @head: head of linked list
+* Description: Go through the list only once.
+* Return: number of nodes in list. If fails, exit with status 98.
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+}
