8ed7aff2e87bdbb0da3382a2c4d44123fe171305
ramii02 <louhibirami11@gmail.com>
2021-04-16 23:42:27 +0200

    hell

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,28 +1,41 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 /**
-* main - main
-* description: adv
-* @argc: argc
-* @argv: argv
-* Return: 0
+* main - prints its own opcodes
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: Always 0 (Success)
 */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+int bytes, i;
+char *arr;
+
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-if (atoi(argv[1]) < 0)
+
+bytes = atoi(argv[1]);
+
+if (bytes < 0)
 {
 printf("Error\n");
 exit(2);
+}
+
+arr = (char *)main;
+
+for (i = 0; i < bytes; i++)
+{
+if (i == bytes - 1)
+{
+printf("%02hhx\n", arr[i]);
+break;
+}
+printf("%02hhx ", arr[i]);
 }
 return (0);
 }
