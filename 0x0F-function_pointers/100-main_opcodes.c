ff90e4b74b8af34841a6c17bf2d67841fc170923
ramii02 <louhibirami11@gmail.com>
2021-04-16 23:37:43 +0200

    heloo

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,24 +5,24 @@
 #include <string.h>
 
 /**
- *  * main - main
- *   * description: adv
- *    * @argc: argc
- *     * @argv: argv
- *      * Return: 0
- *       */
+* main - main
+* description: adv
+* @argc: argc
+* @argv: argv
+* Return: 0
+*/
 
 int main(int argc, char **argv)
 {
-		if (argc != 2)
-				{
-							printf("Error\n");
-									exit(1);
-										}
-			if (atoi(argv[1]) < 0)
-					{
-								printf("Error\n");
-										exit(2);
-											}
-				return (0);
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+if (atoi(argv[1]) < 0)
+{
+printf("Error\n");
+exit(2);
+}
+return (0);
 }
