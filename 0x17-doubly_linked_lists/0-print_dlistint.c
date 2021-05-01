2a90f97c0fa4d46c734bc893feeed601064c3e7b
ramii02 <louhibirami11@gmail.com>
2021-05-01 02:22:37 +0200

    print all element

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* print_dlistint - function that prints all the elements 
+* @h: node.
+* Return: EXIT_SUCCESS.
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+const dlistint_t *est = h;
+size_t i = 0;
+
+while (est)
+{
+printf("%d\n", est->n);
+est = est->next;
+i++;
+}
+return (i);
+}
