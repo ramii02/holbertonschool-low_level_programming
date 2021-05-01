5b63034ac9f6a91d710bbbe7a2f765ba8926fab0
ramii02 <louhibirami11@gmail.com>
2021-05-01 14:33:09 +0200

    hi

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,16 +6,13 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t est= 0;
-const dlistint_t *ule = h;
+int ule = 0;
 
-if (ule)
-return (0);
-while (ule)
+for (ule = 0; h != NULL; ule++)
 {
-printf("%d\n", ule->n);
-ule = ule->next;
-est++;
+printf("%d\n", h->n);
+h = h->next;
 }
-return (est);
+return (ule);
+}
 }
