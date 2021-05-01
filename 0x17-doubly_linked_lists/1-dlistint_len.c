8987e4b26b9689c0ea000f90bcab23c532e542d1
ramii02 <louhibirami11@gmail.com>
2021-05-01 15:08:01 +0200

    return the number of element

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include "list.h"
 
 /**
-* dlistint_len - function that returns the number of elements in a linked 
-* @h: pointer to the constant dlistint_t head
-* Return: the number of nodes
+* dlistint_len - Funtion that returns the number of elements of a list.
+* @h: Head of the list.
+* Return: The times it prints.
 */
 size_t dlistint_len(const dlistint_t *h)
 {
