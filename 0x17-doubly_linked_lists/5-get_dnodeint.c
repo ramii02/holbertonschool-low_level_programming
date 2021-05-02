56567c4937e47ee6579bac0baf71eea66d0c7fd9
ramii02 <louhibirami11@gmail.com>
2021-05-02 23:44:43 +0200

    return the nth node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 /**
 * get_dnodeint_at_index - return a node of double linked list
-* @head: head of the double list to print
+* @head: head of the list
 * @index: number
 * Return: the length of nodes
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 unsigned int i = 0;
-dlistint_t *tmp = head;
+dlistint_t *est = head;
 
 while (index > i)
 {
-tmp = tmp->next;
+est = est->next;
 i++;
 }
-return (tmp);
+return (est);
 }
