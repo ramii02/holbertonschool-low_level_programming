c957565ba6313424a16e7b649a2e7dbf2d631d50
ramii02 <louhibirami11@gmail.com>
2021-07-22 20:30:02 +0200

    c

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,19 +1,20 @@
 #include "binary_trees.h"
-
 /**
-* binary_tree_node - creates a binary tree node
-* @parent: pointer to parent node
-* @value: input value
-* Return: pointer to new node
-*/
+  * binary_tree_node - creates a binary tree node
+  * @parent: pointer to the parent node of the node to create
+  * @value: the value to put in the new node
+  * Return: returns the pointer to the new node of NULL if failure
+  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *btree = NULL;
+	binary_tree_t *newNode;
 
-btree = calloc(1, sizeof(binary_tree_t));
-if (!btree)
-return (NULL);
-btree->parent = parent;
-btree->n = value;
-return (btree);
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->parent = parent;
+	return (newNode);
 }
